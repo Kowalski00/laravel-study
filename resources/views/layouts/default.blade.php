0442115fbe2a649fb4812014b17c740fc1a3310b
kowalski00 <rk0191@hotmail.com>
2025-05-09 16:40:37 -0300

    Adds models and change to vite

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,8 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title','Laravel')</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     @yield('content')
+
+    @vite('resources/js/app.js')
 </body>
 </html>

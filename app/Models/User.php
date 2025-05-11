0018fb2a4c50c21a00a2111aaeb1ef24ee4e128e
kowalski00 <rk0191@hotmail.com>
2025-05-11 10:55:18 -0300

    Adds new models and creates relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,16 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function profile() {
+        return $this->hasOne(Profile::class);
+    }
+
+    public function posts() {
+        return $this->hasMany(Post::class);
+    }
+
+    public function roles() {
+        return $this->belongsToMany(Role::class);
+    }
 }
